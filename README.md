a62a6134d6a12976361af2a1bec9a0f22323b58a
Ben Botsford <ben.botsford@flatironschool.com>
2022-09-13 14:36:21 -0500

    finish v1

--- README.md
+++ README.md
@@ -48,14 +48,14 @@ $ python seed.py
 $ flask run
 ```
 
-- Define a `POST` block inside of the `/baked_goods` route that creates a new
+- Define a `POST` block inside of a `/baked_goods` route that creates a new
   baked good in the database and returns its data as JSON. The request will
   send data in a form.
 - Define a `PATCH` block inside of the `/bakeries/<int:id>` route that updates
   the name of the bakery in the database and returns its data as JSON. As with
   the previous `POST` block, the request will send data in a form. The form
   does not need to include values for all of the bakery's attributes.
-- Define a `DELETE` block inside of the `/baked_goods/<int:id>` route that
+- Define a `DELETE` block inside of a `/baked_goods/<int:id>` route that
   deletes the baked good from the database and returns a JSON message confirming
   that the record was successfully deleted.
 
@@ -66,33 +66,21 @@ submit.
 
 #### `POST /baked_goods`
 
-![POST request with form data for a bakery's name](
-
+![POST request with form data for a baked good's name, price, and bakery_id](
+https://curriculum-content.s3.amazonaws.com/python/building-ppd-api-lab-1.png
 )
-
-```json
-
-```
 
 #### `PATCH /bakeries/<int:id>`
 
 ![PATCH request with form data for a bakery's name](
-
+https://curriculum-content.s3.amazonaws.com/python/building-ppd-api-lab-2.png
 )
-
-```json
-
-```
 
 #### `DELETE /baked_goods/<int:id>`
 
 ![DELETE request for baked good by ID](
-
+https://curriculum-content.s3.amazonaws.com/python/building-ppd-api-lab-3.png
 )
-
-```json
-
-```
 
 ***
 
--- app/testing/app_test.py
+++ app/testing/app_test.py
@@ -2,138 +2,77 @@ import json
 from os import environ
 import re
 
+from flask import request
+
 from app import app
 from models import db, Bakery, BakedGood
 
 class TestApp:
     '''Flask application in flask_app.py'''
 
-    def test_bakeries_route(self):
-        '''has a resource available at "/bakeries".'''
-        response = app.test_client().get('/bakeries')
-        assert(response.status_code == 200)
+    def test_creates_baked_goods(self):
+        '''can POST new baked goods through "/baked_goods" route.'''
 
-    def test_bakeries_route_returns_json(self):
-        '''provides a response content type of application/json at "/bakeries"'''
-        response = app.test_client().get('/bakeries')
-        assert response.content_type == 'application/json'
-
-    def test_bakeries_route_returns_list_of_bakery_objects(self):
-        '''returns JSON representing models.Bakery objects.'''
         with app.app_context():
-            b = Bakery(name="Mr. Bakery")
-            db.session.add(b)
+
+            af = BakedGood.query.filter_by(name="Apple Fritter").first()
+            db.session.delete(af)
             db.session.commit()
 
-            response = app.test_client().get('/bakeries')
-            data = json.loads(response.data.decode())
-            assert(type(data) == list)
-            for record in data:
-                assert(type(record) == dict)
-                assert(record['id'])
-                assert(record['name'])
-                assert(record['created_at'])
+            response = app.test_client().post(
+                '/baked_goods',
+                data={
+                    "name": "Apple Fritter",
+                    "price": 2,
+                    "bakery_id": 5,
+                }
+            )
 
-            db.session.delete(b)
-            db.session.commit()
+            assert(response.status_code == 201)
+            assert(response.content_type == 'application/json')
+            assert(af.id)
 
-    def test_bakery_by_id_route(self):
-        '''has a resource available at "/bakeries/<int:id>".'''
-        response = app.test_client().get('/bakeries/1')
-        assert(response.status_code == 200)
+    def test_updates_bakeries(self):
+        '''can PATCH bakeries through "bakeries/<int:id>" route.'''
 
-    def test_bakery_by_id_route_returns_json(self):
-        '''provides a response content type of application/json at "/bakeries/<int:id>"'''
-        response = app.test_client().get('/bakeries/1')
-        assert response.content_type == 'application/json'
-
-    def test_bakery_by_id_route_returns_one_bakery_object(self):
-        '''returns JSON representing one models.Bakery object.'''
         with app.app_context():
-            b = Bakery(name="Mr. Bakery")
-            db.session.add(b)
+
+            mb = Bakery.query.filter_by(id=1).first()
+            mb.name = "My Bakery"
+            db.session.add(mb)
             db.session.commit()
 
-            response = app.test_client().get('/bakeries/1')
-            data = json.loads(response.data.decode())
-            assert(type(data) == dict)
-            assert(data['id'])
-            assert(data['name'])
-            assert(data['created_at'])
+            response = app.test_client().patch(
+                '/bakeries/1',
+                data = {
+                    "name": "Your Bakery",
+                }
+            )
 
-            db.session.delete(b)
-            db.session.commit()
+            assert(response.status_code == 200)
+            assert(response.content_type == 'application/json')
+            assert(mb.name == "Your Bakery")
 
-    def test_baked_goods_by_price_route(self):
-        '''has a resource available at "/baked_goods/by_price".'''
-        response = app.test_client().get('/baked_goods/by_price')
-        assert(response.status_code == 200)
-    
-    def test_bakery_by_id_route_returns_json(self):
-        '''provides a response content type of application/json at "/baked_goods/by_price"'''
-        response = app.test_client().get('/baked_goods/by_price')
-        assert response.content_type == 'application/json'
+    def test_deletes_baked_goods(self):
+        '''can DELETE baked goods through "baked_goods/<int:id>" route.'''
 
-    def test_baked_goods_by_price_returns_list_of_baked_goods(self):
-        '''returns JSON representing one models.Bakery object.'''
         with app.app_context():
-            b = BakedGood(name="Madeleine", price=10)
-            db.session.add(b)
-            db.session.commit()
+            
+            af = BakedGood.query.filter_by(name="Apple Fritter").first()
+            if not af:
+                af = BakedGood(
+                    name="Apple Fritter",
+                    price=2,
+                    bakery_id=5,
+                )
+                db.session.add(af)
+                db.session.commit()
+            
 
-            response = app.test_client().get('/baked_goods/by_price')
-            data = json.loads(response.data.decode())
-            assert(type(data) == list)
-            for record in data:
-                assert(record['id'])
-                assert(record['name'])
-                assert(record['price'])
-                assert(record['created_at'])
+            response = app.test_client().delete(
+                f'/baked_goods/{af.id}'
+            )
 
-            db.session.delete(b)
-            db.session.commit()
-
-    def test_most_expensive_baked_good_route(self):
-        '''has a resource available at "/baked_goods/most_expensive".'''
-        response = app.test_client().get('/baked_goods/most_expensive')
-        assert(response.status_code == 200)
-
-    def test_most_expensive_baked_good_route_returns_json(self):
-        '''provides a response content type of application/json at "/bakeries/<int:id>"'''
-        response = app.test_client().get('/baked_goods/most_expensive')
-        assert response.content_type == 'application/json'
-
-    def test_most_expensive_baked_good_route_returns_one_baked_good_object(self):
-        '''returns JSON representing one models.BakedGood object.'''
-        with app.app_context():
-            b = BakedGood(name="Madeleine", price=10)
-            db.session.add(b)
-            db.session.commit()
-
-            response = app.test_client().get('/baked_goods/most_expensive')
-            data = json.loads(response.data.decode())
-            assert(type(data) == dict)
-            assert(data['id'])
-            assert(data['name'])
-            assert(data['price'])
-            assert(data['created_at'])
-
-            db.session.delete(b)
-            db.session.commit()
-    
-    def test_most_expensive_baked_good_route_returns_most_expensive_baked_good_object(self):
-        '''returns JSON representing one models.BakedGood object.'''
-        with app.app_context():
-            b = BakedGood(name="Madeleine", price=10)
-            db.session.add(b)
-            db.session.commit()
-
-            response = app.test_client().get('/baked_goods/most_expensive')
-            data = json.loads(response.data.decode())
-            prices = [baked_good.price for baked_good in BakedGood.query.all()]
-            highest_price = max(prices)
-
-            assert(data['price'] == highest_price)
-
-            db.session.delete(b)
-            db.session.commit()
+            assert(response.status_code == 200)
+            assert(response.content_type == 'application/json')
+            assert(not BakedGood.query.filter_by(name="Apple Fritter").first())
