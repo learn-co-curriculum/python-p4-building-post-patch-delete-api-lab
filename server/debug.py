6bd530467d4b8eefc1cf3de94755e67afb060f82
Ben Botsford <ben.botsford@flatironschool.com>
2023-05-03 09:52:28 -0500

    remove debug.py

--- server/debug.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-from app import app
-from models import db, Bakery, BakedGood
-
-if __name__ == '__main__':
-    with app.app_context():
-        import ipdb; ipdb.set_trace()
